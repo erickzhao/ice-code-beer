d2db74e9e889056b727c857f11d4c499d7b1542b
erick <erick@hotmail.ca>
2018-03-16 14:20:50 -0400

    Add shitty csv thing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 
+var csvConverter;csvConverter=csvConverter||{},function(){"use strict";return csvConverter={convert:function(e){var t="object"!=typeof e?JSON.parse(e):e,r=Object.keys(t[0]),n=this.parseHeaders(r,t),o=this.parseBody(t,n);return this.open(o)},parseHeaders:function(e){var t="";return e.forEach(function(e){t+=e+","}),t+="\r\n"},parseBody:function(e,t){var r,n,o;return e.forEach(function(e,c){o="";for(c in e)""!==o&&(o+=","),r=/\,/,"string"==typeof(n=e[c])&&(n=r.test(n)?'"'+n+'"':n),o+=n;t+=o+"\r\n"}),t},open:function(e){if(Object.hasOwnProperty.call(window,"ActiveXObject")&&!window.ActiveXObject){var t=new Blob([e],{type:"text/csv;charset=utf-8;"});return window.navigator.msSaveBlob(t,"tcm-01.csv")}var r="data:text/csv;charset=utf-8,"+escape(e),n=document.createElement("a");return n.setAttribute("href",r),n.setAttribute("download","download.csv"),document.body.appendChild(n),n.click(),window.open(r)}}}.call(this);
+
 class App extends Component {
 
   constructor(props) {
@@ -28,7 +30,13 @@ class App extends Component {
       })
   }
 
+  
+
   render() {
+    const download = () => {
+      const data = JSON.parse(localStorage.getItem('pubs'));
+      csvConverter.convert(data);
+    }
     const gerts = () => {
       if (this.state.PATRICK && this.state.index < this.state.PATRICK.length) {
         this.setState({
@@ -37,7 +45,6 @@ class App extends Component {
         const saved = JSON.parse(localStorage.getItem('pubs')) || [];
         saved.push(this.state.PATRICK[this.state.index]);
         localStorage.setItem('pubs', JSON.stringify(saved));
-        console.log(localStorage.getItem('pubs'));
       }
     }
     const hurts = () => {
@@ -67,6 +74,8 @@ class App extends Component {
         { PATRICK2 }
         <button onClick={hurts}>IT HURTS</button>
         <button onClick={gerts}>IT GERTS</button>
+        <button onClick={download}>DOWNLOAD</button>
+        <br/>
       </div>
     );
   }
