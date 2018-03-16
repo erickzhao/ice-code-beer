0c810661c33ef04767f82bd21a97b3e35b8be098
erick <erick@hotmail.ca>
2018-03-16 13:48:02 -0400

    WOW IT'S SO INTERACTIVE

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { PATRICK: null };
+    this.state = { 
+      PATRICK: null,
+      index: 0
+    };
+  }
 
+  componentWillMount() {
     fetch('/api/PATRICK')
       .then(p => {
         return p.json();
@@ -22,26 +27,39 @@ class App extends Component {
       })
   }
 
+  
+
+  
+
   render() {
+    const increment = () => {
+      if (this.state.PATRICK && this.state.index < this.state.PATRICK.length) {
+        this.setState({
+          index: this.state.index + 1
+        })
+      }
+    }
     console.log(this.state);
-    const { PATRICK } = this.state;
-    const PATRICK2 = PATRICK && PATRICK.map(p => (
+    const { PATRICK, index } = this.state;
+    const PATRICK2 = PATRICK && [PATRICK[index]].map(p => (
       <p className="App-intro">
-        <img src={p.image_url}/>
+        <img className="bar-image" src={p.image_url}/>
         <h1>{p.name}</h1>
-        <p>{p.price}</p>
-        <p>{p.rating}</p>
-        <p>{p.distance}</p>
         <p>{p.location.display_address.join(", ")}</p>
+        <p>{p.price}</p>
+        <p>Rating: {p.rating}</p>
+        <p>Distance: {(p.distance/1000).toFixed(1)} km</p>
       </p>
     ))
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">PATRICK</h1>
+          <h1 className="App-title">FIND BARS TO GO TO ON ST.PATRICK'S DAY</h1>
         </header>
         { PATRICK2 }
+        <button onClick={increment}>Bunk off</button>
+        <button onClick={increment}>Crack on</button>
       </div>
     );
   }
